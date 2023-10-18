555d81e21de9148e84fe1a922f9923205e8c31e7
Allen Zheng <volcaronitee8533@gmail.com>
2023-10-18 13:37:29 -0400

    added middleware

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    "https://localhost:3000",
+    "https://www.nitrackeer.onrender.com",
+    "https://nitrackeer.onrender.com"
+];
+
+module.exports = { allowedOrigins };
--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 const express = require("express");
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOption");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3500;
 
-app.use('/', express.static(path.join(__dirname, "/public")));
+// Add middleware
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use('/', express.static(path.join(__dirname, "public")));
 app.use('/', require("./routes/root"));
 
+// Covers 404 errors
 app.all('*', (req, res) => {
     res.status(404);
-    if (req.accepts("html")) {
+    if (req.accepts("html"))
         res.sendFile(path.join(__dirname, "views", "404.html"));
-    } else if (req.accepts("json")) {
-        res.json({ message: "404 Not Found"});
-    } else {
+    else if (req.accepts("json"))
+        res.json({ message: "404 Not Found" });
+    else
         res.type("txt").send("404 Not Found");
-    }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
