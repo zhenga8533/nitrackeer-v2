672430737cd7b57a818e87f59768a937e798d360
Allen Zheng <volcaronitee8533@gmail.com>
2023-10-18 23:14:36 -0400

    changed configs

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,4 @@
 const allowedOrigins = [
-    'https://www.nitrackeer-api.onrender.com',
-    'https://nitrackeer-api.onrender.com',
     'https://www.nitrackeer.onrender.com',
     'https://nitrackeer.onrender.com'
 ];
